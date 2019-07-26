f6308db3b7110a1c8a9bb5890af69ad2f6df0269
Benjamin Baranger <baranger.benjamin@gmail.com>
2019-07-26 07:49:31 +0100

    replace fake data by dynamic ones

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,5 @@
 class ProductsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
-
+  skip_before_action :authenticate_user!, only: [:index]
   def index
     @products = Product.all
   end
